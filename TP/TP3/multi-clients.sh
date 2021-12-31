07b762d4436c34ef458e84bc6c5449808b7a539d
Elhadji Moussa FAYE <moussafaye.m@gmail.com>
2021-12-31 23:33:29 +0100

    Mise à jour du script de multiclient Le script gére maintenant l'exo 4

--- TP/TP3/multi-clients.sh
+++ TP/TP3/multi-clients.sh
@@ -1,34 +1,44 @@
 #!/bin/bash
 
-#Exo 2
 exo2=exo2-client
 exo3=exo3-client
+exo4=exo4-client
+#Exo 2
 if test -f "$exo2"; then
 	# On crée plusieurs instances de client qui vont soliciter le serveur
 	for i in {1..20}
 	do
 		./$exo2 $i&
 	done
-elif test -f "$exo3"; then #Exo 3
-	echo "test de +"
+elif test -f "$exo3" || test -f "$exo4"; then #Exo 3 et exo 4
+	exo_courant="$exo3"
+	if test -f "$exo4"; then
+		exo_courant="$exo4"
+	fi
+
+	# echo "test de +"
 	for i in {1..20}
 	do
-		./$exo3 $i + $((i*2)) &
+		./$exo_courant $i + $((i*2)) &
 	done
-	echo "test de -"
+
+	# echo "test de *"
 	for i in {1..20}
 	do
-		./$exo3 $i - $((i*2)) &
+		./$exo_courant $i "*" $((i*2)) &
 	done
-	echo "test de *"
+	
+	# echo "test de -"
 	for i in {1..20}
 	do
-		./$exo3 $i "*" $((i*2)) &
+		./$exo_courant $i - $((i*2)) &
 	done
-	echo "test de /"
+
+
+	# echo "test de /"
 	for i in {1..20}
 	do
-		./$exo3 $((i*2)) / $i &
+		./$exo_courant $((i*2)) / $i &
 	done
 fi
 
